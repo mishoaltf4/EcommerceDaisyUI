be41c88da4b9da136974c7afacfe14aefce87925
Mikheil Jioevi <mikheil.jioevi@gau.edu.ge>
2025-01-04 02:32:10 +0400

    added footer, styled footer

--- src/app/components/layout/footer/footer.component.ts
+++ src/app/components/layout/footer/footer.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {faGithub, faInstagram, faYoutube} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
 
+  protected readonly faGithub = faGithub;
+  protected readonly faInstagram = faInstagram;
+  protected readonly faYoutube = faYoutube;
 }
