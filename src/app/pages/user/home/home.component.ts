47e74a3b59e7854fa0fe7e71f3964e06db3d04be
Mikheil Jioevi <mikheil.jioevi@gau.edu.ge>
2025-01-05 15:47:48 +0400

    Added homePage [PROGRESSION] - 90%

--- src/app/pages/user/home/home.component.ts
+++ src/app/pages/user/home/home.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {faTruck} from '@fortawesome/free-solid-svg-icons/faTruck';
+import {faAward} from '@fortawesome/free-solid-svg-icons/faAward';
+import {faShieldHalved} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
+  protected readonly faTruck = faTruck;
+  protected readonly faAward = faAward;
+  protected readonly faShieldHalved = faShieldHalved;
 }
