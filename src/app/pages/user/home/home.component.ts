c7d0f45686d5fe9c82214e6ef55292854930770a
Mikheil Jioevi <mikheil.jioevi@gau.edu.ge>
2025-01-06 15:13:28 +0400

    Displaying products

--- src/app/pages/user/home/home.component.ts
+++ src/app/pages/user/home/home.component.ts
@@ -1,16 +1,26 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {faTruck} from '@fortawesome/free-solid-svg-icons/faTruck';
 import {faAward} from '@fortawesome/free-solid-svg-icons/faAward';
 import {faShieldHalved} from '@fortawesome/free-solid-svg-icons';
+import {ProductService} from '../../../services/product.service';
+import {IProduct} from '../../../interfaces/product.interface';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent{
+
+  products: IProduct[];
+
+  constructor(private productService: ProductService) {
+    this.products = this.productService.getProducts();
+  }
+
 
   protected readonly faTruck = faTruck;
   protected readonly faAward = faAward;
