a3c87ceb76db576b636a7594cae0d78c59d3bbf8
Mikheil Jioevi <mikheil.jioevi@gau.edu.ge>
2025-01-06 14:56:54 +0400

    Added product interface and product servcie

---
+++ src/app/interfaces/product.interface.ts
@@ -0,0 +1,13 @@
+export interface IProduct {
+  id: number,
+  title: string,
+  image: string,
+  rating: number,
+  isStock: boolean,
+  price: number,
+  colors: string[],
+  sizes: string[],
+  description: string,
+  reviews: number[],
+  category: string
+}
