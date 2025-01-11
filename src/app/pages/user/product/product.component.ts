f14f7346e20c3e9b15f32a70b2592c3631917da8
Mikheil Jioevi <mikheil.jioevi@gau.edu.ge>
2025-01-11 15:17:37 +0400

    single product page (60%)

--- src/app/pages/user/product/product.component.ts
+++ src/app/pages/user/product/product.component.ts
@@ -5,18 +5,22 @@ import {ActivatedRoute} from '@angular/router';
 import {CommonModule, NgIf} from '@angular/common';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {faStar} from '@fortawesome/free-solid-svg-icons';
+import {faHeart} from '@fortawesome/free-regular-svg-icons';
+import {DetailComponent} from '../../../components/product/detail/detail.component';
 
 @Component({
   selector: 'app-product',
   imports: [
     CommonModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    DetailComponent
   ],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
 })
 export class ProductComponent implements OnInit{
   product: IProduct | undefined;
+  protected quantity: number = 1;
 
   constructor(private productService: ProductService, private activedRoute: ActivatedRoute){}
 
@@ -25,5 +29,16 @@ export class ProductComponent implements OnInit{
     this.product = this.productService.getProductByID(productID);
   }
 
+  protected quantityIncrement():void{
+    this.quantity++;
+  }
+
+  protected quantityDecrement():void{
+    if(this.quantity != 1){
+      this.quantity--;
+    }
+  }
+
   protected readonly faStar = faStar;
+  protected readonly faHeart = faHeart;
 }
